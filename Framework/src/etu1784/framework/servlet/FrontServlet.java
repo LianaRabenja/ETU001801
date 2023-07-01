d9fc7ccb48b3d2b5d7101fee8afe6e4faffe388f
LianaRabenja <rabenjamampiononaliana@gmail.com>
2023-07-01 18:19:16 +0300

    Sprint Final

---
+++ Framework/src/etu1784/framework/servlet/FrontServlet.java
@@ -0,0 +1,82 @@
+package etu1784.framework.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.lang.reflect.InvocationTargetException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import etu1784.framework.Mapping;
+import util.Util;
+import etu1784.framework.ModelView;
+
+@MultipartConfig()
+public class FrontServlet extends HttpServlet {
+    private PrintWriter out;
+
+    HashMap<String, Mapping> mappingUrls;
+    HashMap<String, Object> singleton;
+    private Util util;
+    private String sessionVariable;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        try {
+
+            this.util = new Util();
+            this.mappingUrls = new HashMap<>();
+            this.singleton = new HashMap<>();
+            final String tomPath = "/WEB-INF/classes/";
+            this.sessionVariable = getInitParameter("session");
+
+            String path = getServletContext().getRealPath(tomPath);
+            util.loadMapping(path, tomPath, mappingUrls, singleton);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String url = request.getRequestURL().toString();
+        url = util.processUrl(url, request.getContextPath());
+
+        try {
+            Mapping map = mappingUrls.get(url);
+
+            if (map == null) throw new Exception("Not Found");
+
+            ModelView mv = util.invokeMethod(request, map, singleton, sessionVariable);
+            if(!mv.isJson()){
+                util.setAttributeRequest(request, mv);
+                request.getRequestDispatcher(mv.getView()).forward(request, response);
+            }else {
+                out = response.getWriter();
+                out.print(mv.toJson());
+            }
+
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException ignored) {
+
+        } catch (ServletException | IOException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
